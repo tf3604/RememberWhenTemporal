0a7314127fa21f924ffada16f2ddcf6cd336135b
Brian Hansen <brian@tf3604.com>
2017-06-09 08:28:27 -0500

    Add MIT license to script files.

--- AutoTrackerDemos/042 - AtTimeZone.sql
+++ AutoTrackerDemos/042 - AtTimeZone.sql
@@ -1,3 +1,10 @@
+-----------------------------------------------------------------------------------------------------------------------
+-- 042 - AtTimeZone.sql
+-- Version 1.0.0
+-- Look for the most recent version of this script at www.tf3604.com/temporal.
+-- MIT License.  See the bottom of this file for details.
+-----------------------------------------------------------------------------------------------------------------------
+
 -- SQL now gives us time zone support.
 select sysdatetime() at time zone 'Central Standard Time' at time zone 'UTC';
 select sysdatetimeoffset() at time zone 'UTC';
@@ -17,3 +24,27 @@ select cast('2017-06-10 10:10:00' as datetime2) at time zone 'Central Standard T
 
 -- We can see the valid times zone.
 select * from sys.time_zone_info;
+
+
+
+-----------------------------------------------------------------------------------------------------------------------
+-- Copyright 2017, Brian Hansen (brian at tf3604.com).
+
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+-- 
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+-- 
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+-----------------------------------------------------------------------------------------------------------------------
