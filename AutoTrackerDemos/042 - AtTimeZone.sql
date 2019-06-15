1a57cd0cf4f18c96f6947ff15bb569db66742353
Brian Hansen <brian@tf3604.com>
2019-06-15 14:55:13 -0500

    Update script for Chattanooga start time.

--- AutoTrackerDemos/042 - AtTimeZone.sql
+++ AutoTrackerDemos/042 - AtTimeZone.sql
@@ -16,12 +16,12 @@ select sysdatetimeoffset() at time zone 'India Standard Time';
 
 -- Input must be a smalldatetime, datetime, datetime2 or datetimeoffset.
 -- A string (that is a valid datetime) will fail.
-select '2017-06-10 10:10:00' at time zone 'Central Standard Time' at time zone 'UTC';
+select '2019-06-22 14:30:00' at time zone 'Eastern Standard Time' at time zone 'UTC';
 --Msg 8116, Level 16, State 1, Line 8
 --Argument data type varchar is invalid for argument 1 of AT TIME ZONE function.
 
 -- Cast it to a valid data type.
-select cast('2017-06-10 10:10:00' as datetime2) at time zone 'Central Standard Time' at time zone 'UTC';
+select cast('2019-06-22 14:30:00' as datetime2) at time zone 'Eastern Standard Time' at time zone 'UTC';
 
 -- We can see the valid times zone.
 select * from sys.time_zone_info;
