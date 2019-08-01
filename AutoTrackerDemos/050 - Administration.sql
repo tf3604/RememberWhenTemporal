6e54f14f0f01a7a0211282ffa03c998476e58b97
Brian Hansen <brian@tf3604.com>
2019-08-01 08:41:02 -0500

    Bump version number.

--- AutoTrackerDemos/050 - Administration.sql
+++ AutoTrackerDemos/050 - Administration.sql
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------------------------------------------------------
 -- 050 - Administration.sql
--- Version 1.0.5
+-- Version 1.0.6
 -- Look for the most recent version of this script at www.tf3604.com/temporal.
 -- MIT License.  See the bottom of this file for details.
 -----------------------------------------------------------------------------------------------------------------------
